a16f5979ed1bdc3f936a76201ee7f449b1e5e2c9
Josh Wilson <joshuef@gmail.com>
2018-12-21 12:46:29 +0000

    planning an implementation

--- src/createNfsList/createNfsList.js
+++ src/createNfsList/createNfsList.js
@@ -1,25 +1,35 @@
 import logger from '../logger';
 import { addEntryToMutable, createMutable } from '../safeNetwork';
 
+import { FilesMap } from 'safe-schema';
+import { shepherd } from 'semantic-shepherd';
+
 import { resolveableMap, safeId } from 'safe-schema';
 import { man, validate } from 'rdf-check-mate';
 import rdflib from 'rdflib';
 
-import rdfMePlz from '../rdfMePlz';
-//
-// pass MD content address
-// if existing... fails (use update to udpate)
-//
-// if non passed is random.
-// can be encrypted
-//
-// pass an array of keyvalue. NFSaddress (resolvable against CAS or DNS):
-//
-//
 
+const fakeDefaultData = {
+    'somefile/path'    : 'safe://1111111',
+    'another/path'     : 'safe://2222222',
+    'another/path/sub' : 'safe://33333'
+}
 // const createNfsList = async ( { mdlocationUri = '', pathsCasArray = [], encrypt = false } ) =>
-export const createNfsList = async ( data ) =>
+export const createNfsList = async ( data = fakeDefaultData ) =>
 {
+    // TODO:
+
+    // Step 1: Create FilesMap.
+        // generate random XOR address for this (or use a target)
+        // use RDF consistent typetag
+
+    // Step 2: Create FilesItem for each file...
+        // 2.1 Convert entries object to target / date / size....
+        // some info may not be needed as XOU URL has it... (mimetype...?)
+
+    // Step 3: Add entry to FilesMap, using KEY as id.
+
+
     // TODO: create NFS list is analogous to create data w/ schema.
 
     // we have data
@@ -27,78 +37,38 @@ export const createNfsList = async ( data ) =>
     // so we parse...
 
     // TODO: this needs to have an ID to be passed to RDFMEPLZ
-    const ourFileList = {
+    const ourFilesMap = {
         version     : "1",
         id          : 'safe://asdadsadsa',
-        ':default'  : 'safe://asdasdasdasdasdsadasdadsa',
-        entriesList : {
-            'somefile/path'    : 'safe://1111111',
-            'another/path'     : 'safe://2222222',
-            'another/path/sub' : 'safe://33333',
-        }
+        default          : 'safe://asdadsadsa',
     };
 
+    //
+    // const ourWebId = {
+    //     id      : 'safe://mywebid.gabriel#me',
+    //     name    : 'Gabriel Viganotti',
+    //     nick    : 'bochaco',
+    //     website : 'safe://mywebsite.gabriel',
+    //     image   : 'safe://mywebsite.gabriel/images/myavatar',
+    // };
 
-    const ourWebId = {
-        id      : 'safe://mywebid.gabriel#me',
-        name    : 'Gabriel Viganotti',
-        nick    : 'bochaco',
-        website : 'safe://mywebsite.gabriel',
-        image   : 'safe://mywebsite.gabriel/images/myavatar',
-    };
-
-    logger.trace( 'Creationinnnnn', ourFileList );
+    logger.trace( 'Creationinnnnn NFS', ourFilesMap );
     // Testing with:
 
     //safe schema defaultValue...
-    await man( resolveableMap );
+    const result = await man( FilesMap );
 
-    // specifics of our file list over resolveable map...
-    // each is a file...
-    // do we want metadata here?
-
-    // await validate( safeId, ourWebId );
-    await validate( resolveableMap, ourFileList );
+    logger.trace('man outcome',result)
 
 
-    // do generic conversion using map.
+    const valid =  await validate( FilesMap, ourFilesMap );
+    logger.trace('validity check outcome',valid)
 
-
-    // why is this the other way round?
-    // let graph = await rdfMePlz( ourWebId, safeId );
-    //
-    // // logger.trace( 'valid±±!!!', graph, ourFileList.id );
-    //
-    // rdflib.serialize( null, graph, ourFileList.id, 'text/turtle', ( err, result ) =>
-    // // rdflib.serialize( null, graph, ourFileList.id, 'application/ld+json', ( err, result ) =>
-    // {
-    //     if( err )
-    //     {
-    //         logger.error( '!!!!!!!!!!!!!!!!!errorrrr', err )
-    //         throw new Error( err );
-    //     }
-    //
-    //     logger.info( 'SERLIALISEDDDDD' )
-    //     console.log( result )
-    // } );
-    //
-    let anothergraph = await rdfMePlz( ourFileList, resolveableMap );
-
-    //
-    // const someMD = {
-    //     thing : 'stuff',
-    //     more  : 'other'
-    // }
-    //
-    // save( someMD );
-
-    // save(ourFileList).schema(resolveableMap);
+    let rdfObj = await shepherd( ourFilesMap, FilesMap );
 
 
 
-
-    rdflib.serialize( null, anothergraph, ourFileList.id, 'text/turtle', ( err, result ) =>
-    // rdflib.serialize( null, graph, ourFileList.id, 'application/ld+json', ( err, result ) =>
+    rdflib.serialize( null, rdfObj, ourFilesMap.id, 'text/turtle', ( err, result ) =>
     {
         if( err )
         {
@@ -111,22 +81,6 @@ export const createNfsList = async ( data ) =>
     } );
 
 
-    // return null;
-    // return createMutable( data );
-    // let md;
-    //
-    // if( ! options.mdlocationUri )
-    // {
-    //     md = await createMutable();
-    //
-    // }
-    // else
-    // {
-    //     // setup the MD/RDF from location.
-    //     md = await createMutable();
-    // }
-    //
-    // return md;
 }
 
 export const addNfsListing = async ( md, key, value ) =>
